a98e7ccff1b14857c372c4bd3243dd00b4ad88c0
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-25 03:32:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,15 @@ class Song
   end
 
   def self.genre_count
-    @@genres.count
+    genre_hash={}
+    @@genres.each do |genre|
+      if genre_hash[genre]
+        genre_hash[genre]+=1
+      else
+        genre_hash[genre]=1
+      end
+    end
+    genre_hash
   end
 
   def self.artist_count
